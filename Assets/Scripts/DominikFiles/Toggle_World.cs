fa47a3f1064a598e9558ff5ca68f95f1f280d529
DominikBreuer <dominikbreuer15@gmail.com>
2026-04-25 01:08:55 +0200

    Toggle Test bestanden

--- Assets/Scripts/DominikFiles/Toggle_World.cs
+++ Assets/Scripts/DominikFiles/Toggle_World.cs
@@ -1,6 +1,27 @@
 using UnityEngine;
 
-public class Toggle_World
+public class Toggle_World: MonoBehaviour
 {
-    
+    [Header("Welten Zuweisung")]
+    public GameObject Light_World;
+    public GameObject Shadow_World;
+
+    private bool isWorldActive = true;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            ToggleWorld();
+        }
+    }
+    public void ToggleWorld()
+    {
+        isWorldActive = !isWorldActive;
+
+        Light_World.SetActive(isWorldActive);
+        Shadow_World.SetActive(!isWorldActive);
+
+        Debug.Log("Aktive Welt : " + isWorldActive);
+    }
 }
