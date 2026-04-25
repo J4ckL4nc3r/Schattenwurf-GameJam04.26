d02424b0319fdc9be9626c858319492af01f9fe3
Student <student@sae.de>
2026-04-25 14:49:05 +0200

    Fixed floor models again xd

--- Assets/Scripts/PascalFiles/MapTilingBehavior.cs
+++ Assets/Scripts/PascalFiles/MapTilingBehavior.cs
@@ -49,7 +49,7 @@ public class MapTilingBehavior : MonoBehaviour
 
     private void SpawnNewTile(int tileID, int tilePrefabID)
     {
-        GameObject tile = Instantiate(tilePrefabs[tilePrefabID], new Vector3(_curChunkID.x + (tileID * tileSize), transform.position.y, 0), Quaternion.identity);
+        GameObject tile = Instantiate(tilePrefabs[tilePrefabID], new Vector3(_curChunkID.x + (tileID * tileSize), transform.position.y, 0), transform.rotation);
         tile.transform.parent = transform;
         _activeTiles[tileID+2] = tile;
     }
